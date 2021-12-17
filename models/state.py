9e196e6a4f0532d6e1a443c4859fbf3c1da92311
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-17 21:23:54 +0100

    Update State model

--- models/state.py
+++ models/state.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
-    name = Column(String(128), nullable = False)
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
     cities = relationship(
         "City",
-        cascade = "all, delete, delete-orphan",
-        back_populates = "state"
-    )
+        cascade="all, delete, delete-orphan",
+        back_populates="state"
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+
+    @property
+    def cities(self):
+        """Returns the cities in this State"""
+        from models import storage
+        cities_in_state = []
+        for value in storage.all(City).values():
+            if value.state_id == self.id:
+                cities_in_state.append(value)
+        return cities_in_state
