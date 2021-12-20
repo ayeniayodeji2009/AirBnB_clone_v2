56ec4f013de67ec94dd3f3e67385e2468dfb364f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-20 21:23:31 +0100

    Update Place and Amenity models

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -14,5 +15,8 @@ class Amenity(BaseModel, Base):
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     place_amenities = relationship(
-        'Place'
+        'Place',
+        secondary=place_amenity,
+        viewonly=False,
+        back_populates='amenities'
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
