7c64266c52159974a4c55578a034b8e1f91703fc
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-21 06:03:24 +0100

    Update Place model

--- models/place.py
+++ models/place.py
@@ -89,6 +89,13 @@ class Place(BaseModel, Base):
                     amenities_of_place.append(value)
             return amenities_of_place
 
+        @amenities.setter
+        def amenities(self, value):
+            """Adds an amenity to this Place"""
+            if type(value) is Amenity:
+                if value.id not in self.amenity_ids:
+                    self.amenity_ids.append(value.id)
+
     @property
     def reviews(self):
         """Returns the reviews of this Place"""
@@ -98,21 +105,3 @@ class Place(BaseModel, Base):
             if value.place_id == self.id:
                 reviews_of_place.append(value)
         return reviews_of_place
-
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-        # @property
-        # def amenities(self):
-        #     """Returns the amenities of this Place"""
-        #     from models import storage
-        #     amenities_of_place = []
-        #     for value in storage.all(Amenity).values():
-        #         if value.id in self.amenity_ids:
-        #             amenities_of_place.append(value)
-        #     return amenities_of_place
-
-        @amenities.setter
-        def amenities(self, value):
-            """Adds an amenity to this Place"""
-            if type(value) is Amenity:
-                if value.id not in self.amenity_ids:
-                    self.amenity_ids.append(value.id)
