b42d175e8919a81c2f99aaf80e42e7a0b130e4fc
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-21 07:44:46 +0100

    Update Place model

--- models/place.py
+++ models/place.py
@@ -76,7 +76,7 @@ class Place(BaseModel, Base):
             'Amenity',
             secondary=place_amenity,
             viewonly=False,
-            back_populates='place_amenities'
+            backref='place_amenities'
         )
     else:
         @property
