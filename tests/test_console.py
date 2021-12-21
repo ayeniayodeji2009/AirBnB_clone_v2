2df1b0d52e123234700fc30bb9fa9f3fce413878
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-21 14:33:35 +0100

    Update tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,6 +13,7 @@ from console import HBNBCommand
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
 from tests import clear_stream
 
 
@@ -374,5 +375,33 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertIn('123', result)
             self.assertIn('john25@gmail.com', cout.getvalue())
             self.assertIn('123', cout.getvalue())
+            cursor.close()
+            dbc.close()
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
+    def test_db_count(self):
+        """Tests the count command with the database storage.
+        """
+        with patch('sys.stdout', new=StringIO()) as cout:
+            cons = HBNBCommand()
+            dbc = MySQLdb.connect(
+                host=os.getenv('HBNB_MYSQL_HOST'),
+                port=3306,
+                user=os.getenv('HBNB_MYSQL_USER'),
+                passwd=os.getenv('HBNB_MYSQL_PWD'),
+                db=os.getenv('HBNB_MYSQL_DB')
+            )
+            cursor = dbc.cursor()
+            cursor.execute('SELECT COUNT(*) FROM states;')
+            res = cursor.fetchone()
+            prev_count = int(res[0])
+            cons.onecmd('create State name="Enugu"')
+            clear_stream(cout)
+            cons.onecmd('count State')
+            cnt = cout.getvalue().strip()
+            self.assertEqual(int(cnt), prev_count + 1)
+            clear_stream(cout)
+            cons.onecmd('count State')
             cursor.close()
             dbc.close()
