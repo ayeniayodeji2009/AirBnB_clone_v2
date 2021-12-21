83e9dc8becc63ee5d4152a51c3faa158f34b8538
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-21 20:53:49 +0100

    Update State model.

--- models/state.py
+++ models/state.py
@@ -14,18 +14,19 @@ class State(BaseModel, Base):
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    cities = relationship(
-        'City',
-        cascade='all, delete, delete-orphan',
-        backref='state'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
-
-    @property
-    def cities(self):
-        """Returns the cities in this State"""
-        from models import storage
-        cities_in_state = []
-        for value in storage.all(City).values():
-            if value.state_id == self.id:
-                cities_in_state.append(value)
-        return cities_in_state
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City',
+            cascade='all, delete, delete-orphan',
+            backref='state'
+        )
+    else:
+        @property
+        def cities(self):
+            """Returns the cities in this State"""
+            from models import storage
+            cities_in_state = []
+            for value in storage.all(City).values():
+                if value.state_id == self.id:
+                    cities_in_state.append(value)
+            return cities_in_state
