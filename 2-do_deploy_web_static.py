0d678b657d091c4b029b8629563f0ba04d759164
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-13 19:02:26 +0100

    Update deployment script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """A module for web application deployment with Fabric."""
-import fabric.api as fabric_api
 import os
+from fabric.api import env, put, run
 
 
-fabric_api.env.hosts = ["34.73.0.174", "34.75.208.81"]
+env.hosts = ["34.73.0.174", "34.75.208.81"]
 """The list of host server IP addresses."""
 
 
@@ -20,20 +20,14 @@ def do_deploy(archive_path):
     folder_path = "/data/web_static/releases/{}/".format(folder_name)
     success = False
     try:
-        fabric_api.put(archive_path, "/tmp/")
-        fabric_api.run("mkdir -p {}".format(folder_path))
-        fabric_api.run(
-            "tar -xzf /tmp/{} -C {}".format(file_name, folder_path)
-        )
-        fabric_api.run("rm /tmp/{}".format(file_name))
-        fabric_api.run(
-            "mv {}web_static/* {}".format(folder_path, folder_path)
-        )
-        fabric_api.run("rm -rf {}web_static".format(folder_path))
-        fabric_api.run("rm -rf /data/web_static/current")
-        fabric_api.run(
-            "ln -s {} /data/web_static/current".format(folder_path)
-        )
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
         success = True
     except Exception:
         success = False
