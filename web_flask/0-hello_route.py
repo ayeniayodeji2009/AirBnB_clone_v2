aafac1440538ca76afb343d31026e0d47570d3d5
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-29 06:11:25 +0100

    Update Flask web application script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,10 @@ from flask import Flask
 
 app = Flask(__name__)
 '''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
     '''The home page.'''
     return 'Hello HBNB!\n'
