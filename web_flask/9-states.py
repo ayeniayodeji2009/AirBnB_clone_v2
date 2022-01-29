da8953485bc655ddc75ae8d99741597fe3fc63af
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-29 17:18:13 +0100

    Change template region conditions

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -19,19 +19,23 @@ def states(id=None):
     states = None
     state = None
     all_states = list(storage.all(State).values())
+    case = 404
     if id is not None:
         res = list(filter(lambda x: x.id == id, all_states))
         if len(res) > 0:
             state = res[0]
             state.cities.sort(key=lambda x: x.name)
+            case = 2
     else:
         states = all_states
         for state in states:
             state.cities.sort(key=lambda x: x.name)
         states.sort(key=lambda x: x.name)
+        case = 1
     ctxt = {
         'states': states,
-        'state': state
+        'state': state,
+        'case': case
     }
     return render_template('9-states.html', **ctxt)
 
